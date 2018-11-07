9b8e6fe17643c0a4308b95c86f04b98350773ce3
Memo Leon <gleonoliva@users.noreply.github.com>
2018-11-06 23:30:44 -0600

    Added basic validation
    Implemented
    - Password minimum length
    - Password containing at least 3 repeated characters
    - Password containing sequential alphanumeric characters

--- pwndpasslib/src/main/java/org/androidpass/pwndpasslib/Verifier.java
+++ pwndpasslib/src/main/java/org/androidpass/pwndpasslib/Verifier.java
@@ -8,6 +8,8 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -15,9 +17,95 @@ public class Verifier {
 
     private static final String API_URL = "https://api.pwnedpasswords.com/range/";
 
+    private static final int MINIMUM_PASSWORD_LENGTH = 8;
 
-    public boolean verify(String password) {
+    public enum Validity {
+        VALID,
+        TOO_SHORT,
+        DICTIONARY_WORD,
+        REPETITIVE_CHARACTERS,
+        SEQUENTIAL_CHARACTERS,
+        BREACHED
+    }
 
+
+    public Validity verify(String password) {
+        if (password.length() <= MINIMUM_PASSWORD_LENGTH) {
+            return Validity.TOO_SHORT;
+        }
+
+        if (isDictionaryWord(password)) {
+            return Validity.DICTIONARY_WORD;
+        }
+
+        if (containsRepetitiveCharacters(password)) {
+            return Validity.REPETITIVE_CHARACTERS;
+        }
+
+        if (containsSequentialCharacters(password)) {
+            return Validity.SEQUENTIAL_CHARACTERS;
+        }
+
+        if (hasPasswordBreached(password)) {
+            return Validity.BREACHED;
+        }
+
+        return Validity.VALID;
+    }
+
+    private boolean containsSequentialCharacters(String password) {
+        Pattern p = Pattern.compile("(\\w)+");
+        Matcher m = p.matcher(password);
+
+        while(m.find()) {
+            String substring = m.group();
+            if (isSequential(substring, true) || isSequential(substring, false)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isSequential(String substring, boolean ascending) {
+        char[] chars = substring.toCharArray();
+        int limit = 3;
+
+        for(int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            boolean found = true;
+            for (int j = 1; j < limit && i + j < chars.length; j++) {
+                char d = (char) (ascending ? c + j : c - j);
+                if (chars[i + j] != d || !Character.isLetterOrDigit(c)) {
+                    found = false;
+                    break;
+                }
+            }
+
+            if (found) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean containsRepetitiveCharacters(String password) {
+        Pattern p = Pattern.compile("(.)\\1\\1");
+        Matcher m = p.matcher(password);
+
+        if (m.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isDictionaryWord(String password) {
+
+        return false;
+    }
+
+    private boolean hasPasswordBreached(String password) {
         try {
             String passwordHash = computeSha1(password);
             String hashPrefix = passwordHash.substring(0, 5);
--- pwndpasslib/src/test/java/org/androidpass/pwndpasslib/ExampleUnitTest.java
+++ pwndpasslib/src/test/java/org/androidpass/pwndpasslib/ExampleUnitTest.java
@@ -10,14 +10,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
 
     @Test
     public void testWithPassword() {
         Verifier v = new Verifier();
-        assertTrue(v.verify("password"));
+        assertTrue(v.verify("password") == Verifier.Validity.BREACHED);
+    }
+
+    @Test
+    public void testAscendingSequence() {
+        Verifier v = new Verifier();
+        assertTrue(v.verify("qwpeoriuabc=--") == Verifier.Validity.SEQUENTIAL_CHARACTERS);
+    }
+
+    @Test
+    public void testDescendingSequence() {
+        Verifier v = new Verifier();
+        assertEquals(v.verify("=4321{}-0asdiotn"), Verifier.Validity.SEQUENTIAL_CHARACTERS);
     }
 }
