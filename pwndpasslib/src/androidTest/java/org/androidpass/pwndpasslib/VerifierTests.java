b6a7c8ba4e673f7d90cbf0727f8afc9662a6d0d2
Memo Leon <gleonoliva@users.noreply.github.com>
2018-11-12 00:23:11 -0600

    Added more dictionaries

---
+++ pwndpasslib/src/androidTest/java/org/androidpass/pwndpasslib/VerifierTests.java
@@ -0,0 +1,18 @@
+package org.androidpass.pwndpasslib;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class VerifierTests {
+
+    @Test
+    public void testDictionaryWord() {
+        Verifier v = new Verifier();
+        v.verify("=-apple-=", validity -> { assertEquals(validity, Verifier.Validity.DICTIONARY_WORD); });
+    }
+}
